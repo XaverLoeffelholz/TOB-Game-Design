b533a995b8a876955f9acdb5cc3103bc3791581d
XaverLoeffelholz <xaver@loeffelholz.de>
2015-08-26 10:38:02 +0800

    Able to shoot platforms

---
+++ TOB project/Assets/Initiation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initiation : MonoBehaviour {
+	public GameObject platform;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Instantiate (platform, new Vector3 (i * 2.0F, 0, j * 2.0F), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TOB project/Assets/ShootingPlatforms.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShootingPlatforms : MonoBehaviour {
+
+	//define maximum Distance where platforms can be shot
+	public float maximumDistance; 
+
+	Camera camera;
+
+
+
+	private bool shootPlatform = false;
+	private bool destroyPlatform = false;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("MainCamera").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			shootPlatform = true;
+		} 
+
+		if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			shootPlatform = false;
+		} 
+
+		if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			destroyPlatform = true;
+		} 
+		
+		if (Input.GetKeyUp (KeyCode.Mouse1)) {
+			destroyPlatform = false;
+		} 
+
+
+
+		if (shootPlatform || destroyPlatform) {
+			Ray ray = camera.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, maximumDistance)) {
+				Debug.Log ("geht noch");
+				if (hit.collider.CompareTag ("platform")) {
+					if (shootPlatform) {
+						createPlatformFunction (hit.collider.gameObject);
+					} else {
+						destroyPlatformFunction (hit.collider.gameObject);
+					}
+				}
+
+			} 
+		}
+	
+	}
+
+
+	private void createPlatformFunction(GameObject platform) {
+		platform.GetComponent<MeshRenderer> ().enabled = true;
+	}
+
+	private void destroyPlatformFunction(GameObject platform) {
+		platform.GetComponent<MeshRenderer> ().enabled = false;
+	}
+
+}
