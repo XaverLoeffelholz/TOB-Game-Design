3f45c06db5d9db17fa5e444b312ecbeb86d3cec3
XaverLoeffelholz <xaver@loeffelholz.de>
2015-09-09 16:21:30 +0800

    higher platforms + check connections

---
+++ TOB project/Assets/pathConnections.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pathConnections : MonoBehaviour {
+	public Transform start;
+	public Transform end;
+	private NavMeshPath path;
+
+	// Use this for initialization
+	void Start () {
+		path = new NavMeshPath ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			NavMesh.CalculatePath(start.position, end.position, NavMesh.AllAreas, path);
+			if (path.status == NavMeshPathStatus.PathPartial) {
+				Debug.Log ("way is partial");
+			} else if (path.status == NavMeshPathStatus.PathComplete) {
+				Debug.Log ("way is complete");
+			} else {
+				Debug.Log ("error");
+			}
+		}
+	}
+}
