58854c87699a645fe409159ff87d19ad1a684a15
kkchew <k_k_chew@yahoo.com>
2015-10-03 19:31:18 +0800

    Adding laser effect for the gun
    - Shoot a green laser while creating a platform.
    - Shoot a red laser while destroying a platform.

--- TOB project/Assets/Scripts/shootingPlatforms.cs
+++ TOB project/Assets/Scripts/shootingPlatforms.cs
@@ -8,20 +8,56 @@ public class shootingPlatforms : MonoBehaviour {
 
 	Camera cameraObject;
 
+	private bool hitPlatform = false;
 	private bool shootPlatform = false;
 	private bool destroyPlatform = false;
 
 	private GameObject currentPreview;
     private int destroyedPlatforms;
 
+	private float timer;
+	private GameObject laserGun;
+	private LineRenderer laserLine;
+	private float laserDisplayTime = 0.2f;
+	private Color laserBuildColor = new Color(0f, 1f, 0f);
+	private Color laserDestroyColor = new Color(1f, 0f, 0f);
+
 	// Use this for initialization
 	void Start () {
         destroyedPlatforms = 0;
 		cameraObject = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+		laserGun = GameObject.FindGameObjectWithTag ("Gun");
+		laserLine = laserGun.GetComponent<LineRenderer> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// accumulate the time and this will be used to disable the laser effect
+		timer += Time.deltaTime;
+
+		// disable the laser effect if necessary
+		if (timer >= laserDisplayTime) {
+			laserLine.enabled = false;
+			// reset the timer
+			timer = 0f;
+		}
+
+		// set up raycast to check what gameObject is being hit
+		Ray ray = cameraObject.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+		RaycastHit hit;
+		// if the raycast hit something
+		if (Physics.Raycast (ray, out hit, maximumDistance)) {
+			// if the raycast hit a platform
+			if (hit.collider.CompareTag ("platform")) {
+				hitPlatform = true;
+			} else {
+				hitPlatform = false;
+			}
+		// if the raycast did not hit anything
+		} else {
+			hitPlatform = false;
+		}
+
 		// based on the mouse input, decide whether the platform is being shot or destroyed
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			shootPlatform = true;
@@ -36,40 +72,77 @@ public class shootingPlatforms : MonoBehaviour {
 			destroyPlatform = false;
 		} 
 
-		// set up raycast to check what gameObject is being hit
-		Ray ray = cameraObject.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
-		RaycastHit hit;
-		//currentPreview.GetComponent<platformScript>().unpreview();
-		if (Physics.Raycast (ray, out hit, maximumDistance)) {
-			if (hit.collider.CompareTag ("platform")) {
-				// get the state of the platform
-				string platformState = "";
-				platformState = hit.collider.gameObject.GetComponent<platformScript>().getState();
-				// get the connectivity of the platform
-				bool connected = false;
-				connected = hit.collider.gameObject.GetComponent<platformScript>().checkConnection();
+		// set the first point of the line renderer to the gun position
+		laserLine.SetPosition(0, laserGun.transform.position);
 
-				// if the platform is inactive and connected, preview it
+		// set the second point of the line renderer to where the raycast hit or to infinity if the raycast did not hit anything
+		if (shootPlatform && hitPlatform) {
+			// display the laser
+			laserLine.material.color = laserBuildColor;
+			laserLine.enabled = true;
+			// set the second point of the line renderer to the where the raycast hit
+			laserLine.SetPosition (1, hit.point);
+			// build the platform
+		} else if (shootPlatform && !hitPlatform) {
+			// display the laser
+			laserLine.material.color = laserBuildColor;
+			laserLine.enabled = true;
+			// set the second point of the line renderer to "infinity"
+			laserLine.SetPosition(1, ray.origin + ray.direction * maximumDistance);
+		} else if (destroyPlatform && hitPlatform) {
+			// display the laser
+			laserLine.material.color = laserDestroyColor;
+			laserLine.enabled = true;
+			// set the second point of the line renderer to the where the raycast hit
+			laserLine.SetPosition (1, hit.point);
+			// build the platform
+		} else if (destroyPlatform && !hitPlatform) {
+			// display the laser
+			laserLine.material.color = laserDestroyColor;
+			laserLine.enabled = true;
+			// set the second point of the line renderer to "infinity"
+			laserLine.SetPosition(1, ray.origin + ray.direction * maximumDistance);
+		}
+
+		// if the raycast hit a platform
+		if (hitPlatform) {
+			string platformState = "";
+			platformState = hit.collider.gameObject.GetComponent<platformScript> ().getState ();
+			// get the connectivity of the platform
+			bool connected = false;
+			connected = hit.collider.gameObject.GetComponent<platformScript> ().checkConnection ();
+
+			// preview the platform if not shoot or destroy platform
+			if (!shootPlatform && !destroyPlatform) {
+				// if the platform is in an inactive state and connected, preview it
 				if ((platformState == "inactive") && connected) {
-					//currentPreview = hit.collider.gameObject;
-					hit.collider.gameObject.GetComponent<platformScript>().preview();
+					// preview the platform
+					hit.collider.gameObject.GetComponent<platformScript> ().preview ();
 				}
-				// if platform is preview, shot and connected, build it
-				else if ((platformState == "preview") && shootPlatform && connected) {
+			}
+			// if shoot platform
+			else if (shootPlatform) {
+				// if platform is in a preview state and connected, build it
+				if ((platformState == "preview") && connected) {
 					Debug.Log ("platform built");
-					hit.collider.gameObject.GetComponent<platformScript>().build();
+					// build the platform
+					hit.collider.gameObject.GetComponent<platformScript> ().build ();
 				}
-				// if platform is active and destroyed
-				else if ((platformState == "active") && destroyPlatform) {
+			}
+			// if destroy platform
+			else if (destroyPlatform) {
+				// if platform is in an active state
+				if (platformState == "active") {
 					// if the platform can be destroyed, destroy it
-					if (hit.collider.gameObject.GetComponent<platformScript>().indestructable == false) {
+					if (hit.collider.gameObject.GetComponent<platformScript> ().indestructable == false) {
 						Debug.Log ("platform destroyed");
 						destroyedPlatforms++;
-						hit.collider.gameObject.GetComponent<platformScript>().destroy();  
+						// destroy the platform
+						hit.collider.gameObject.GetComponent<platformScript> ().destroy ();  
 					}
 				}
 			}
-		} 
+		}
 	}
 
 	// get the number of destroyed platforms
