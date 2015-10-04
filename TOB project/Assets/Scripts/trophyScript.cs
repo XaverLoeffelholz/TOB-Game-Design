1d1820b3fa635784ceef15ec39d05f429b90a056
XaverLoeffelholz <xaver@loeffelholz.de>
2015-10-04 13:33:17 +0800

    Level 1 and 2 built

---
+++ TOB project/Assets/Scripts/trophyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trophyScript : MonoBehaviour
+{
+    private Transform trophy;
+
+    // Use this for initialization
+    void Start()
+    {
+        trophy = this.transform.GetChild(0).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trophy.Rotate(0, 25.0f * Time.deltaTime, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trophy Collected!!!");
+    }
+}
