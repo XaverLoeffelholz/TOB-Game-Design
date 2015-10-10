cff2b002257cdc53f393d3f91ca809b04dab8b1a
XaverLoeffelholz <xaver@loeffelholz.de>
2015-10-10 18:01:46 +0800

    Menu Fixed

---
+++ TOB project/Assets/Scripts/ShowScoreMainMenu.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowScoreMainMenu : MonoBehaviour {
+    public int level;
+
+	// Use this for initialization
+	void Start () {
+        if (level == 1)
+        {
+            int score1 = PlayerPrefs.GetInt("dragonTrophy");
+            if (score1 == 0)
+            {
+                this.transform.parent.GetComponent<Image>().enabled = false;
+                this.GetComponent<Text>().enabled = false;
+            }
+            else
+            {
+                this.transform.parent.GetComponent<Image>().enabled = true;
+                this.GetComponent<Text>().text = score1.ToString();
+                this.GetComponent<Text>().enabled = true;
+            }
+        }
+        else if (level == 2)
+        {
+            int score2 = PlayerPrefs.GetInt("airportTrophy");
+            if (score2 == 0)
+            {
+                this.transform.parent.GetComponent<Image>().enabled = false;
+                this.GetComponent<Text>().enabled = false;
+            }
+            else
+            {
+                this.transform.parent.GetComponent<Image>().enabled = true;
+                this.GetComponent<Text>().text = score2.ToString();
+                this.GetComponent<Text>().enabled = true;
+            }
+        }
+        else if (level == 3)
+        {
+            int score3 = PlayerPrefs.GetInt("merlionTrophy");
+            if (score3 == 0)
+            {
+                this.transform.parent.GetComponent<Image>().enabled = false;
+                this.GetComponent<Text>().enabled = false;
+            }
+            else
+            {
+                this.transform.parent.GetComponent<Image>().enabled = true;
+                this.GetComponent<Text>().text = score3.ToString();
+                this.GetComponent<Text>().enabled = true;
+            }
+        }
+        else if (level == 4)
+        {
+            int score4 = PlayerPrefs.GetInt("singaTrophy");
+            if (score4 == 0)
+            {
+                this.transform.parent.GetComponent<Image>().enabled = false;
+                this.GetComponent<Text>().enabled = false;
+            }
+            else
+            {
+                this.transform.parent.GetComponent<Image>().enabled = true;
+                this.GetComponent<Text>().text = score4.ToString();
+                this.GetComponent<Text>().enabled = true;
+            }
+        }
+        else if (level == 5)
+        {
+            int score5 = PlayerPrefs.GetInt("mbsTrophy");
+            if (score5 == 0)
+            {
+                this.transform.parent.GetComponent<Image>().enabled = false;
+                this.GetComponent<Text>().enabled = false;
+            }
+            else
+            {
+                this.transform.parent.GetComponent<Image>().enabled = true;
+                this.GetComponent<Text>().text = score5.ToString();
+                this.GetComponent<Text>().enabled = true;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+    }
+}
--- TOB project/Assets/Scripts/platformEvent.cs
+++ TOB project/Assets/Scripts/platformEvent.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class platformEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler 
+public class platformEvent : MonoBehaviour  
 {
-	public GameObject trophy;
+
+    //IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+
+    public GameObject trophy;
 	public GameObject platform;
 	public int level;
 
@@ -12,9 +15,9 @@ public class platformEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 	private Color highlightColor = new Color(0.1f, 1f, 1f, 0.6f);
 	private Color platformColor;
 
-	void Start () {
-        PlayerPrefs.DeleteAll();
+    private bool hover = false;
 
+	void Start () {
         // set the time scale back to 1 whenever the main menu is loaded within the game
         // this will ensure that all the subsequent scenes will be loaded properly
         Time.timeScale = 1f;
@@ -22,6 +25,57 @@ public class platformEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 		platformRenderer = platform.GetComponent<MeshRenderer> ();
 		platformColor = platformRenderer.material.color;
 	}
+
+    void OnMouseOver()
+    {
+        hover = true;
+    }
+
+    void Update()
+    {
+
+        if (hover)
+        {
+            // rotate the trophy to make it more spectacular
+            trophy.GetComponent<trophyRotation>().enabled = true;
+            // change the platform color to highlight color
+            platformRenderer.material.color = highlightColor;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                // load a level on mouse click
+                Application.LoadLevel("Level" + level.ToString());
+            }
+        } else { 
+            // stop the rotation of the trophy
+            if (trophy != null)
+            {
+                trophy.GetComponent<trophyRotation>().enabled = false;
+            }
+            // change the platform color back to its original color
+            platformRenderer.material.color = platformColor;
+        }
+    }
+
+    void LateUpdate()
+    {
+        hover = false;
+    }
+
+    void onMouseExit()
+    {
+        // stop the rotation of the trophy
+        if (trophy != null)
+        {
+            trophy.GetComponent<trophyRotation>().enabled = false;
+        }
+        // change the platform color back to its original color
+        Debug.Log("In On MOuse Exit!");
+        platformRenderer.material.color = platformColor;
+    }
+
+
+    /*
 
 	public void OnPointerEnter(PointerEventData eventData) {
 		// rotate the trophy to make it more spectacular
@@ -56,6 +110,9 @@ public class platformEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 		} else if (level == 5) {
 			PlayerPrefs.SetInt ("mbsTrophy", 1);
 		}
-        */
-	}
+        
+	}*/
+
+
+
 }
