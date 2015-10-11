a06d1639f07349051a58283b4523323aa98fac37
kkchew <k_k_chew@yahoo.com>
2015-10-11 14:34:02 +0800

    Now you can use escape button to stop the cutscene.

--- TOB project/Assets/Scripts/movieManager.cs
+++ TOB project/Assets/Scripts/movieManager.cs
@@ -5,14 +5,29 @@ public class movieManager : MonoBehaviour {
 	public MovieTexture movie;
 
 	private Renderer movieRenderer;
+	private bool stop;
 
 	// Use this for initialization
 	void Start () {
+		stop = false;
 		// set the movie as a texture
 		movieRenderer = this.GetComponent<MeshRenderer> ();
 		movieRenderer.material.mainTexture = movie as MovieTexture;
 		movie.Play ();
 	}	
+
+	// Update is called once per frame
+	void Update () {
+		// if the Escape button is pressed, stop the cutscene
+		if (Input.GetKey (KeyCode.Escape)) {
+			stop = true;
+		}
+
+		// stop the cutscene if stop is true
+		if (stop) {
+			stopMovie ();
+		}
+	}
 
 	// play the cutscene
 	public void playMovie () {
